1a8ce1440cd31d33f7b30a314c29e3c3e2037e6c
chaudhur <chaudhur@enst.fr>
2018-06-14 17:49:34 +0200

    tmp

---
+++ hackadac/src/pulpino.c
@@ -0,0 +1,10 @@
+
+M={RAM,ROM,IRAM,UART,GPIO,SPIM,Timer,Intr,I2C,FLL,CTRL,DBG}
+A={PROC,JTAG,SPI,GPIO,PINS}
+S0=
+for i in M
+	scan(P0,i)
+	store results
+	scan(P1,i)
+	store results
+	compare ane verify assertions
