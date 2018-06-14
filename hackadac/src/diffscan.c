941a80b4fb2fef599890a7f527842c385f290b7a
chaudhur <chaudhur@enst.fr>
2018-06-14 12:13:13 +0200

    tmp

---
+++ hackadac/src/diffscan.c
@@ -0,0 +1,9 @@
+
+M={M0,M1,...} // Memory Zones
+A={A0,A1,...} //Attacker Models
+S={S0,S1,...} // Security Objectives
+P={P0={A0},P1={A1,A2,A3}}
+for i in M
+	in parallel:
+	scan(P,i); check_assertions(S);
+
